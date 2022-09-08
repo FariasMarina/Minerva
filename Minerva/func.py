7a389067b5831e0d4f9bf0b088ecc9ce4b14d690
Joilson-Julkoski <joilson7548@gmail.com>
2022-09-08 00:16:43 -0300

    Funções de piada e do financeiro adicionadas e problema de importação resolvido

--- Minerva/func.py
+++ Minerva/func.py
@@ -1,6 +1,6 @@
 import webbrowser
+import random
 
-# from main import receber_variaveis
 
 
 # - Mudar para Mozilla caso não haja Chrome
@@ -16,10 +16,19 @@ def pesquisar_youtube(comando):
     webbrowser.open(f'https://www.youtube.com/results?search_query={comando}')
     return f'Pesquisei {comando} no Youtube.'
 
-def exemplo(text):
+def fale(text):
     return text
 
 def apresentar(text):
+	# TODO: Colar o texto que esta la nos testes
     return "Olá me chamo Minerva, sou sua assitente virtual open source e estou a sua disposição"
+	
 
+def piadas(text):
+	piadas = ['Duas galinhas estão na cozinha fazendo café. Uma delas pergunta: Pó pô pó?A outra responde: Pó pô.','No primeiro dia de aula, uma aluna nova se apresenta à professora:- Meu nome é Jaqueline, tenho 13 anos e já namoro.- Já o quê?- Jaqueline.', 'O que o lápis disse para o papel? Você está me desapontando','Soy paraguaio e vim para matar-te. Para o quê? Paraguaio.','Duas galinhas estão na cozinha fazendo café. Uma delas pergunta: Pó pô pó?A outra responde: Pó pô.', 'Eu tinha um pintinho chamado Relam. Toda vez que chovia, Relam piava.',' O que o martelo foi fazer no culto de domingo?- Pregar.',' Qual foi o peixe que caiu do 15º andar? Aaaaaaaaaatum.', 'No restaurante, o garçom pergunta: O que deseja para beber? Caipirinha? Não, obrigado, não bebo destilado. Ah, sem problemas. Posso servir pelo outro lado.', 'O paciente chega no consultório e o médico diz: Bom dia, tudo bem? Tudo sim, Doutor. Ótimo, então já pode ir embora.','Um filho diz para a mãe: Mãe, tenho uma festa de 15 anos para ir. Mas não dá para ficar só um dia? 15 anos é muito tempo.', 'Por que a planta não responde? Porque ela é mudinha.','Um amigo agradece ao outro: Muito obrigado. Disponha. Ponha.',' O que a esposa do Albert Einstein disse quando o viu sem roupa? Uau, que físico.', 'Por que o jacaré tirou o filho da escola?...Porque ele réptil de ano!','Qual é a diferença entre a pizza e a sua opinião?A pizza eu pedi! ','Qual é o animal que não vale mais nada? O javali! ']
+	#TODO: Revisar piadas, ver se todas estão separadas no lugar certo,
+	#TODO: ver um jeito de fazer ela esperar um pouco antes de dar a resposta 
+	return random.choice(piadas)
+
+#=== FINCEIRO ===
 
--- Minerva/main.py
+++ Minerva/main.py
@@ -4,6 +4,7 @@ import azure.cognitiveservices.speech as speechsdk
 import speech_recognition as sr
 
 from func import *
+import financeiro
 
 def talk(text):
     speech_config = speechsdk.SpeechConfig(subscription="9bc9a7005e8f4ab9b42c4ecc13d5680a",
@@ -11,12 +12,13 @@ def talk(text):
 
     #In this sample we are using the default speaker
     #Learn how to customize your speaker using SSML in Azure Cognitive Services Speech documentation
-
+	
     speech_config.speech_synthesis_language = 'pt-br'
     audio_config = AudioOutputConfig(use_default_speaker=True)
     synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, 
 											  audio_config=audio_config)
     synthesizer.speak_text_async(text)
+
     #synthesize_to_speaker()
 
 def receber_variaveis(text):
@@ -25,34 +27,39 @@ def receber_variaveis(text):
     return valor
 
 def recebendo_audio():
-    rec = sr.Recognizer()
-    with sr.Microphone(device_index=18) as mic:
-        rec.adjust_for_ambient_noise(mic)
-        print("Estou te ouvindo...")
-        audio = rec.listen(mic)
-        try:
-            frase = rec.recognize_google(audio, language="pt-BR")
-        except:
-            print("Erro no recebimento de áudio")
-        return frase.lower()
-
+    #rec = sr.Recognizer()
+    #with sr.Microphone(device_index=18) as mic:
+    #    rec.adjust_for_ambient_noise(mic)
+    #    print("Estou te ouvindo...")
+    #    audio = rec.listen(mic)
+    #    try:
+    #        frase = rec.recognize_google(audio, language="pt-BR")
+    #    except:
+    #        print("Erro no recebimento de áudio")
+    #    return frase.lower()
+	return input("Digite seu comando: ")
 
 def run_minerva():
-    #comando = recebendo_audio()
+    comando = recebendo_audio()
 
-    comando = input("Digite seu comando: ")
-
-    lista_comandos = {'fale': talk,
-					  'teste': exemplo,
+    lista_comandos = {'fale': fale,
 					  'pesquise': pesquisar_google,
-					  'apresenta': apresentar}
+					  'apresenta': apresentar,
+					  'piada': piadas,
+					  'adicionar conta': financeiro.adicionar, 
+                      #TODO: testar comandos com o reconhecimento de voz
+					  #TODO: editar contas
+					  'deletar conta': financeiro.deletar,
+                      'total de gastos': financeiro.soma_total_mes 
+}
 
     for i in lista_comandos.keys():
         if i in comando:
             resposta = lista_comandos[i]((comando.replace(i+" ", "")))
             talk(resposta)
 
-run_minerva()
+while True:
+	run_minerva()
 
 
 
